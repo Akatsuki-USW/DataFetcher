22eb4789b5359b63552a34605c0499db031b91de
JJH <ashee022@naver.com>
2023-07-03 21:08:45 +0900

    seoul_locationName_fatch와 연계되는 congestion_fetch 수정.

--- getApi/apps/management/commands/congestion_fetch.py
+++ getApi/apps/management/commands/congestion_fetch.py
@@ -11,12 +11,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         apikey = '6f6169666c61736838387a74747569'
-        pk_values = ['강남 MICE 관광특구', '동대문 관광특구', '명동 관광특구', '이태원 관광특구', '잠실 관광특구', '종로·청계 관광특구', '홍대 관광특구', '경복궁·서촌마을',
-                     '광화문·덕수궁', '창덕궁·종묘', '가산디지털단지역', '강남역', '건대입구역', '고속터미널역', '교대역', '구로디지털단지역', '서울역', '선릉역', '신도림역',
-                     '신림역', '신촌·이대역', '역삼역', '연신내역', '용산역', '왕십리역', 'DMC(디지털미디어시티)', '창동 신경제 중심지', '노량진', '낙산공원·이화마을',
-                     '북촌한옥마을', '가로수길', '성수카페거리', '수유리 먹자골목', '쌍문동 맛집거리', '압구정로데오거리', '여의도', '영등포 타임스퀘어', '인사동·익선동',
-                     '국립중앙박물관·용산가족공원', '남산공원', '뚝섬한강공원', '망원한강공원', '반포한강공원', '북서울꿈의숲', '서울대공원', '서울숲공원', '월드컵공원',
-                     '이촌한강공원', '잠실종합운동장', '잠실한강공원']
+        pk_values = ['강남 MICE 관광특구', '동대문 관광특구', '명동 관광특구', '이태원 관광특구', '잠실 관광특구', '종로·청계 관광특구',
+                     '홍대 관광특구', '광화문·덕수궁', '보신각', '서울 암사동 유적', '창덕궁·종묘', '가산디지털단지역', '강남역', '건대입구역',
+                     '고덕역', '고속터미널역', '교대역', '구로디지털단지역', '구로역', '군자역', '남구로역', '대림역', '동대문역', '뚝섬역',
+                     '미아사거리역', '발산역', '북한산우이역', '사당역', '삼각지역', '서울대입구역', '선릉역', '성신여대입구역', '수유역',
+                     '신논현역·논현역', '신도림역', '신림역', '신촌·이대역', '양재역', '역삼역', '연신내역', '오목교역·목동운동장', '왕십리역',
+                     '이태원역', '장지역', '장한평역', '천호역', '총신대입구(이수)역', '충정로역', '합정역', '혜화역', '홍대입구역(2호선)',
+                     '회기역', '4·19 카페거리', '가락시장', '가로수길', '광장(전통)시장', '김포공항', '낙산공원·이화마을', '노량진',
+                     '덕수궁길·정동길', '방배역 먹자골목', '서촌', '성수카페거리', '수유리 먹자골목', '쌍문동 맛집거리', '압구정로데오거리',
+                     '연남동', '영등포 타임스퀘어', '용리단길', '이태원 앤틱가구거리', '인사동·익선동', '창동 신경제 중심지', '청담동 명품거리',
+                     '해방촌·경리단길', 'DDP(동대문디자인플라자)', 'DMC(디지털미디어시티)', '강서한강공원', '고척돔', '광나루한강공원', '난지한강공원',
+                     '노들섬', '뚝섬한강공원', '망원한강공원', '불광천', '서울광장', '서울대공원', '서울숲공원', '아차산', '양화한강공원', '응봉산',
+                     '이촌한강공원', '잠실한강공원', '잠원한강공원', '청계산']
         city_data_list = []
         realtime_congestions =  []
 
@@ -24,21 +30,23 @@ class Command(BaseCommand):
         congestion_mapping = {
             '여유': 'RELAX',
             '보통': 'NOMAL',
-            '약간 붐빔': 'BUZZ',
-            '붐빔': 'VERY_BUZZ'
+            '약간 붐빔': 'BUZZING',
+            '붐빔': 'BUZZING'
         }
         date_format = '%Y-%m-%d %H:%M:%S'
 
         def get_data(city_name):
             url = f'http://openapi.seoul.go.kr:8088/{apikey}/xml/citydata/1/5/{city_name}'
             response = requests.get(url)
+            print(response)
 
             # xml을 딕셔너리로 변환
             congestion_data = xmltodict.parse(response.content)
 
-            # 데이터 추출 . 먼저 지역이름, 혼잡도, 혼잡도 메세지만 가져옴
+            # 데이터 추출 . 먼저 지역이름, 혼잡도, apiId
             area_data = congestion_data['SeoulRtd.citydata']['CITYDATA']['LIVE_PPLTN_STTS']['LIVE_PPLTN_STTS']
             area_name = congestion_data['SeoulRtd.citydata']['CITYDATA']['AREA_NM']
+            api_id = congestion_data['SeoulRtd.citydata']['CITYDATA']['AREA_CD']
             congestion_level = area_data['AREA_CONGEST_LVL']
 
             observed_at = parse_datetime(area_data['PPLTN_TIME'])
@@ -46,6 +54,8 @@ class Command(BaseCommand):
 
             # location_id 값 찾기
             location = Location.objects.get(name=area_name)
+            location, created = Location.objects.get_or_create(name=area_name)
+
             location_id = location.location_id
 
             # 혼잡도를 숫자로 매핑하기.
@@ -55,15 +65,16 @@ class Command(BaseCommand):
                 congestion_level_mapped = None
 
             return {
-                'location_id': location_id,  # location_id 추가
-                'congestion_level': congestion_level_mapped,
-                'observed_at': observed_at_str,
+                 'location_id': location_id,  # location_id 추가
+                 'congestion_level': congestion_level_mapped,
+                 'observed_at': observed_at_str,
+
             }
 
         with ThreadPoolExecutor(max_workers=5) as executor:
             realtime_congestions = list(executor.map(get_data, pk_values))
 
-        print(realtime_congestions)
+        #print(realtime_congestions)
 
         realtime_converter = realtime_convert.ConvertCongestionData(realtime_congestions)
         realtime_converter.handle()
