aca00669b32aa4180268af0bcc30e79b1a245cb4
JJH <ashee022@naver.com>
2023-07-13 21:50:58 +0900

    스케줄러에 daily_congestion_statistic 추가 매주 월요일 00:30

--- getApi/apps/management/commands/schedule_congestion_fetch.py
+++ getApi/apps/management/commands/schedule_congestion_fetch.py
@@ -6,14 +6,20 @@ import os
 
 def congestion_fetch():
     os.system('python3 manage.py congestion_fetch')
+def daily_congestion_statistic():
+    os.system('python3 manage.py daily_congestion_statistic')
 
 class Command(BaseCommand):
     help = 'Runs a scheduler to fetch congestion data'
 
     def handle(self, *args, **options):
         scheduler = BackgroundScheduler()
-        trigger = CronTrigger(minute="21", hour="9-23")
-        scheduler.add_job(congestion_fetch, trigger)
+        trigger1 = CronTrigger(minute="21", hour="9-23")
+        scheduler.add_job(congestion_fetch, trigger1)
+
+        trigger2 = CronTrigger(minute="30", hour="0", day_of_week="mon")
+        scheduler.add_job(daily_congestion_statistic, trigger2)
+
         scheduler.start()
         self.stdout.write(self.style.SUCCESS('스케줄러 시작'))
 
