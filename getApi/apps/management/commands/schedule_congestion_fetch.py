7ac8a4c1115c88992aef64cdbc98c34658f5b482
JJH <ashee022@naver.com>
2023-07-20 14:19:50 +0900

    scheduler 스레드 반영.

--- getApi/apps/management/commands/schedule_congestion_fetch.py
+++ getApi/apps/management/commands/schedule_congestion_fetch.py
@@ -2,43 +2,33 @@ from django.core.management.base import BaseCommand
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 import os
+import threading
 
-
-def congestion_fetch():
-    os.system('python3 manage.py congestion_fetch')
-def daily_congestion_statistic():
-    os.system('python3 manage.py daily_congestion_statistic')
-
-def weekly_congestion_statisti():
-    os.system('python3 manage.py weekly_congestion_statistic')
-
-def sk_congestion_fetch():
-    os.system('python3 manage.py sk_congestion_fetch')
+def run_management_command(command_name):
+    os.system(f'python3 manage.py {command_name}')
 
 class Command(BaseCommand):
-    help = 'Runs a scheduler to fetch congestion data'
+    help = 'schedule_congestion_fetch'
 
     def handle(self, *args, **options):
         scheduler = BackgroundScheduler()
-        trigger1 = CronTrigger(minute="21", hour="9-23")
-        scheduler.add_job(congestion_fetch, trigger1)
 
-        trigger2 = CronTrigger(minute="30", hour="0")
-        scheduler.add_job(daily_congestion_statistic, trigger2)
+        jobs = [
+            {"function": "congestion_fetch", "minute": "21", "hour": "9-23"},
+            {"function": "daily_congestion_statistic", "minute": "30", "hour": "0"},
+            {"function": "weekly_congestion_statisti", "minute": "30", "hour": "0", "day_of_week": "mon"},
+            {"function": "sk_congestion_fetch", "minute": "21", "hour": "9-23"}
+        ]
 
-        trigger3 = CronTrigger(minute="30", hour="0", day_of_week="mon")
-        scheduler.add_job(weekly_congestion_statisti, trigger3)
-
-        trigger4 = CronTrigger(minute="21", hour="9-23")
-        scheduler.add_job(sk_congestion_fetch, trigger4)
+        for job in jobs:
+            trigger = CronTrigger(**{key: job.get(key) for key in ['minute', 'hour', 'day_of_week'] if job.get(key)})
+            scheduler.add_job(run_management_command, trigger, kwargs={"command_name": job["function"]})
 
         scheduler.start()
         self.stdout.write(self.style.SUCCESS('스케줄러 시작'))
 
         try:
-            while True:
-                pass
+            threading.Event().wait()
         except KeyboardInterrupt:
-            # 스케줄러 정지
             scheduler.shutdown()
             self.stdout.write(self.style.SUCCESS('스케줄러 끝'))
