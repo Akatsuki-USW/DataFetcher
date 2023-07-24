ca04e374a0c2425dcfe2d336c7ad13d061505620
JJH <ashee022@naver.com>
2023-07-24 21:50:08 +0900

    server run시 스케줄러 작동

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secrets.Django_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
