65e8f2b522fac484d36ab669bd43524544cbc2e3
JJH <ashee022@naver.com>
2023-08-01 23:50:02 +0900

    주간 통계 orm filter사용 삭제

--- getApi/apps/management/commands/weekly_congestion_statistic.py
+++ getApi/apps/management/commands/weekly_congestion_statistic.py
@@ -6,9 +6,6 @@ from datetime import datetime, timedelta
 class Command(BaseCommand):
     help = "weekly_congestion_statistic.py"
     def handle(self, *args, **options):
-        relax_level = next(filter(lambda x: x[1]=="RELAX", Congestion.CONGESTION_LEVEL_CHOICES))[0]
-        normal_level = next(filter(lambda x: x[1]=="NORMAL", Congestion.CONGESTION_LEVEL_CHOICES))[0]
-        buzz_level = next(filter(lambda x: x[1]=="BUZZ", Congestion.CONGESTION_LEVEL_CHOICES))[0]
 
         # 이게 days=7인지 days=6인지 햇갈리네요 . >> 7일이 맞는걸로 확인.
         end_date = datetime.now().date()
@@ -17,7 +14,7 @@ class Command(BaseCommand):
         result = (
             Congestion.objects
             .filter(observed_at__range=(start_date, end_date))
-            .filter(congestion_level__in=[relax_level, normal_level, buzz_level])
+            .filter(congestion_level__in=[1, 2, 3])
             .values('location_id')
             .annotate(avg_congestion_level=Avg('congestion_level'))
         )
