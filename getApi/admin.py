d5c4eef6dfb55c2da97ca8686a1fa2b8e5795d0e
JJH <ashee022@naver.com>
2023-09-04 22:31:24 +0900

    django_admin 주석처리

--- getApi/admin.py
+++ getApi/admin.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
-from getApi.models import Congestion,Location,DailyCongestionStatistic,WeeklyCongestionStatistic
-
-admin.site.register(Congestion)
-admin.site.register(Location)
-admin.site.register(DailyCongestionStatistic)
-admin.site.register(WeeklyCongestionStatistic)
+# from django.contrib import admin
+# from getApi.models import Congestion,Location,DailyCongestionStatistic,WeeklyCongestionStatistic
+#
+# admin.site.register(Congestion)
+# admin.site.register(Location)
+# admin.site.register(DailyCongestionStatistic)
+# admin.site.register(WeeklyCongestionStatistic)
 
 
 # Register your models here.
